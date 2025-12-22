5210941464795ad8edf014507a50e52007f00a7f
geddadalakshman <geddadalakshman1@gmail.com>
2025-12-22 23:46:49 +0530

    added tfvars file

--- variables/vars.tf
+++ variables/vars.tf
@@ -5,3 +5,10 @@ variable "test-var" {
 output "test-var-output" {
     value = var.test-var
 }
+
+# variable from .tfvars file
+variable "test-var-tfvars" {}
+
+output "test-var-tfvars-output" {
+    value = var.test-var-tfvars
+}
